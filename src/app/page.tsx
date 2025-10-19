6e3abf8856d29689a7fa0e017826c75d1d56c48f
Zel9278 <countbaster@gmail.com>
2025-10-19 15:36:56 +0900

    maybe first

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col items-center sm:items-center">
+        <h1>c30.life</h1>
+        <p>I don't have the energy to make a website.</p>
+        <p>ced</p>
+      </main>
+    </div>
+  );
+}
