7e2984d9709dc4d0b12030774014b1d128139d66
Zel9278 <43949163+Zel9278@users.noreply.github.com>
2026-09-05 08:32:45 +0900

    change fedilink order

--- fediverseLinks.ts
+++ fediverseLinks.ts
@@ -11,9 +11,9 @@ export type FediverseLink = {
 }
 
 export const misskeyAccounts: FediverseAccount[] = [
-  { host: "mk.c30.life", userId: "c30" },
   { host: "misskey.art", userId: "c30" },
   { host: "mk-juice.dev", userId: "c30" },
+  { host: "mk.c30.life", userId: "c30" },
   { host: "misskey.hatachanoima.net", userId: "c30" },
   { host: "eth.rumiserver.com", userId: "c30" },
   { host: "msky.haibala.com", userId: "c30" },
